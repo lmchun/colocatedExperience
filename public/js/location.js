714630f36c219c48ef85fd634f2fe1c3f7ead00d
lmchun <lmchun@sbcglobal.net>
2023-04-24 12:10:01 -0400

    starting to test that users are registered

--- public/js/location.js
+++ public/js/location.js
@@ -1,5 +1,7 @@
 			const userLocation = document.querySelector("[data-location]")
-            if (navigator.geolocation) {
+            let lat = "0",
+		        long = "0";
+			if (navigator.geolocation) {
 				//alert("Allowed");
 
 				navigator.geolocation.watchPosition(successCallback, errorCallback, {});
@@ -8,12 +10,9 @@
 					// alert(currentPosition.coords.longitude +" " + currentPosition.coords.latitude);
                     userLocation.innerHTML=`${currentPosition.coords.longitude} ${currentPosition.coords.latitude}`
 				
-				var lat = currentPosition.coords.latitude,
+				lat = currentPosition.coords.latitude,
 		        long = currentPosition.coords.longitude;
- 
-		        var mapElem = document.getElementById('map');
-				const googleMap = "https://maps.googleapis.com/maps/api/staticmap?center=" +  lat + ',' + long +"&zoom=18&size=400x400&markers=color:red%7Clabel:DoyerSt%7C"+  lat + ',' + long +"&key=AIzaSyCAirwcBgqDzPj6DlLDNzWvF-DPlcjsXfE"
-		    	mapElem.innerHTML = `<img src="${googleMap}" />`					
+				
 				}
 
 				function errorCallback(e) {
@@ -27,3 +26,10 @@
         function logLocation(){
             console.log("logging location")
         }
+
+		function showonMap(){
+			console.log("show map")
+			let mapElem = document.getElementById('map');
+				const googleMap = "https://maps.googleapis.com/maps/api/staticmap?center=" +  lat + ',' + long +"&zoom=18&size=400x400&markers=color:red%7Clabel:DoyerSt%7C"+  lat + ',' + long +"&key=AIzaSyCAirwcBgqDzPj6DlLDNzWvF-DPlcjsXfE"
+		    	mapElem.innerHTML = `<img src="${googleMap}" />`	
+		}
